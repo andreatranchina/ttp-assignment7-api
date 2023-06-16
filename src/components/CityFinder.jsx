3e0d9b591cb682bac4bbe5cf783cccfe9805f123
Andrea <Andrea.Tranchina82@gmail.com>
2023-06-16 13:37:27 -0400

    component folder added

---
+++ src/components/CityFinder.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+
+function CityFinder(){
+    const [zipCode, setZipCode] = useState("");
+
+    // useEffect(() => {
+    //     async function getCities(){
+    //         const citiesList = 
+    //     }
+    // })
+
+
+    return(
+        <div>
+            Hi
+        </div>
+    )
+
+}
+
+export default CityFinder;
